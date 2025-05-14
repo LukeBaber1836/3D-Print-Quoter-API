ce621a8c7ccbb41f5655f50643f64ce610871923
Luke Baber <luke.baber1@gmail.com>
2025-05-14 21:39:55 +0000

    fixed main.py uvicorn command

--- app/main.py
+++ app/main.py
@@ -18,4 +18,4 @@ async def root():
     return {"message": "3D Printing Slicer API"}
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("src.api:app", host="0.0.0.0", port=8000, reload=True)
